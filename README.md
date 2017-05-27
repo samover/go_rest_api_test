088f4463038a8cc29d2dcdcc0daa3e949b6d4406
samover <samuel.overloop@gmail.com>
2017-05-27 12:08:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+Based on tutorial: https://semaphoreci.com/community/tutorials/building-and-testing-a-rest-api-in-go-with-gorilla-mux-and-postgresql
---
+++ migrations/1495878735-products.sql
@@ -0,0 +1,21 @@
+-- Migration: products
+-- Created at: 2017-05-27 11:52:15
+-- ====  UP  ====
+
+BEGIN;
+    CREATE TABLE products
+    (
+        id SERIAL,
+        name TEXT NOT NULL,
+        price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
+        CONSTRAINT products_pkey PRIMARY KEY (id)
+    );
+
+COMMIT;
+
+-- ==== DOWN ====
+
+BEGIN;
+    DROP TABLE products
+
+COMMIT;
